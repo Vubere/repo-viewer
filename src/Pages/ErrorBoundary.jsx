7b779efe8930de65cd6c1cd2a65f92a0812e98f8
Victor Ubere <victorubere@gmail.com>
2022-11-05 20:43:59 +0100

    error page styling

--- src/Pages/ErrorBoundary.jsx
+++ src/Pages/ErrorBoundary.jsx
@@ -1,39 +1,39 @@
-import React from "react"
+import React from "react";
 
-import { redirect } from "react-router-dom"
+import { redirect } from "react-router-dom";
 
-
-
-class ErrorBoundary extends React.Component{
-  constructor(props){
-    super(props)
-    this.goBackToHome = this.goBackToHome.bind()
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.goBackToHome = this.goBackToHome.bind();
     this.state = {
       error: false,
-    }
+    };
   }
-  static getDerivedStateFromError(){
-    
-    return {error:true}
+  static getDerivedStateFromError() {
+    return { error: true };
   }
-  componentDidCatch(_, errorMessage){
-    console.log(errorMessage)
+  componentDidCatch(_, errorMessage) {
+    console.log(errorMessage);
   }
-  goBackToHome(){
-    window.location.replace(window.location.origin)
+  goBackToHome() {
+    window.location.replace(window.location.origin);
   }
- 
-  
-  render(){
-    document.title = 'error'
-    const {error} = this.state
-    return error?(
+
+  render() {
+    document.title = "error";
+    const { error } = this.state;
+    return error ? (
       <section className="errorPage">
-        <p>An error occurred, go back to home page</p>
-        <button onClick={this.goBackToHome}>Home</button>
+        <div>
+          <p>An error occurred. Go back to home page</p>
+          <button onClick={this.goBackToHome}>Home</button>
+        </div>
       </section>
-    ):this.props.children
+    ) : (
+      this.props.children
+    );
   }
 }
 
-export default ErrorBoundary
+export default ErrorBoundary;
